ebc8eb173bdce2ebd76015dd13637bbf75547518
Łukasz Mierzwa <l.mierzwa@gmail.com>
2013-07-07 21:28:52 +0200

    start task

---
+++ upaas_client/start.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+"""
+    :copyright: Copyright 2013 by Łukasz Mierzwa
+    :contact: l.mierzwa@gmail.com
+"""
+
+
+from plumbum import cli
+
+from slumber.exceptions import SlumberHttpBaseException
+
+from upaas.cli.base import UPaaSApplication
+
+from upaas_client.main import ClientApplication
+from upaas_client.return_codes import ExitCodes
+
+
+@ClientApplication.subcommand('start')
+class Start(UPaaSApplication):
+
+    DESCRIPTION = "Start application"
+
+    name = None
+
+    @cli.switch(["n", "name"], str, help="Application name", mandatory=True)
+    def set_name(self, name):
+        self.name = name
+
+    def main(self):
+        self.setup_logger()
+        self.log.info("Getting app '%s' details" % self.name)
+
+        self.api_connect(self.parent.config.server.login,
+                         self.parent.config.server.apikey,
+                         self.parent.config.server.url)
+
+        try:
+            resp = self.api.application.get(name=self.name)
+        except SlumberHttpBaseException, e:
+            self.handle_error(e)
+        else:
+            if not resp.get('objects'):
+                self.log.error("No such application registered: "
+                               "%s" % self.name)
+                return ExitCodes.notfound_error
+
+            app = resp['objects'][0]
+
+            try:
+                self.api.application(app['id']).start.put(
+                    {'name': app['name']})
+            except SlumberHttpBaseException, e:
+                self.handle_error(e)
+            else:
+                self.log.info("Start task queued")
