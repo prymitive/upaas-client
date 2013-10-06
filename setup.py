e4df3371652edc92099dfa33c0afee7c033738fa
Łukasz Mierzwa <l.mierzwa@gmail.com>
2013-10-06 13:09:15 +0200

    prepare for packaging

---
+++ setup.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+    :copyright: Copyright 2013 by Łukasz Mierzwa
+    :contact: l.mierzwa@gmail.com
+"""
+
+
+from setuptools import setup, find_packages
+
+try:
+    from pip.req import parse_requirements
+    required = parse_requirements('requirements.txt')
+except ImportError:
+    required = []
+
+
+setup(
+    name='upaas-client',
+    version='0.1-dev1',
+    license='GPLv3',
+    description='uPaaS CLI client',
+    author='Łukasz Mierzwa',
+    author_email='l.mierzwa@gmail.com',
+    url='https://github.com/prymitive/upaas-client',
+    packages=find_packages(),
+    classifiers=[
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Intended Audience :: System Administrators',
+        'Programming Language :: Python',
+    ],
+    platforms=['Linux'],
+    install_requires=[str(r.req) for r in required],
+)
