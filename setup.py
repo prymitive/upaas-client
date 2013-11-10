ebf9ae68ec09bd6f6b87403e12082703b3a114c4
Łukasz Mierzwa <l.mierzwa@gmail.com>
2013-11-10 12:22:59 +0100

    try to make requires.io happy

--- setup.py
+++ setup.py
@@ -10,9 +10,10 @@ from setuptools import setup, find_packages
 
 try:
     from pip.req import parse_requirements
-    required = parse_requirements('requirements.txt')
+    required = {'install_requires': [str(r.req) for r in parse_requirements(
+        'requirements.txt')]}
 except ImportError:
-    required = []
+    required = {}
 
 
 setup(
@@ -30,6 +31,6 @@ setup(
         'Programming Language :: Python',
     ],
     platforms=['Linux'],
-    install_requires=[str(r.req) for r in required],
     scripts=['upaas'],
+    **required
 )
