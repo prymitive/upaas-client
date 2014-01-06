3c18a354f2455d2e8930c9c70d3960c1c248dfb6
Łukasz Mierzwa <l.mierzwa@gmail.com>
2014-01-06 16:21:38 +0100

    0.2.1 release, only bumping deps

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ except ImportError:
 
 setup(
     name='upaas-client',
-    version='0.2',
+    version='0.2.1',
     license='GPLv3',
     description='uPaaS CLI client',
     author='Łukasz Mierzwa',
