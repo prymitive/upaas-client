f7a901410002fada6b09888d6069a62e3f0fbbae
Łukasz Mierzwa <l.mierzwa@gmail.com>
2014-05-11 10:44:48 +0200

    bump plumbum

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ except ImportError:
 
 setup(
     name='upaas-client',
-    version='0.3',
+    version='0.3.1-dev',
     license='GPLv3',
     description='uPaaS CLI client',
     author='Łukasz Mierzwa',
